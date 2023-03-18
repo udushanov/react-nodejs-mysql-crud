fa5aac6bbc7dfda29a0e7581127e30c3c31ae44f
Ulugbek <u.dushanov@gmail.com>
2023-03-18 06:41:25 +0500

    feat: add get and post methods

--- server/index.js
+++ server/index.js
@@ -1,18 +1,51 @@
 import express from 'express'
-import mysql from 'mysql'
+import mysql from 'mysql2'
 
 const app = express()
 
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: '5130921a',
   database: 'test'
 })
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.json('hello backend')
 })
+
+app.get('/books', (req, res) => {
+  db.query('SELECT * FROM books', (err, data) => {
+    if (err) return res.json(data)
+    return res.json(data)
+  })
+})
+
+app.post('/books', (req, res) => {
+  const values = [req.body.title, req.body.desc, req.body.cover]
+  db.query('INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)', [values], (err, data) => {
+    if (err) return res.json(err)
+    return res.json('Book has been created succesfully')
+  })
+})
+
+/* checking with postman 
+new hhtp request
+method: post, address: 'localhost:8800/books'
+
+to check post method from user
+method: post, Body raw JSON
+in body we should write our json
+{
+  "title": "title from client",
+  "desc": "desc from client",
+  "cover": "cover from client"
+}
+
+app.use(express.json()) //allows us to send method post from user (sending json file from cilent)
+*/
 
 app.listen(8800, () => {
   console.log('The server is running')
